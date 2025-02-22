4a777067552717485cbfd93a2d92f5a64905fc93
GK <gokula_krishnan@trimble.com>
2025-02-22 10:56:45 +0530

    Updates

---
+++ precache-manifest.55c09a4615892708c371df62ca4b889d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "148406030c0320adaa3f",
+    "url": "/CT-2025-UI/static/css/main.1b9aae5f.chunk.css"
+  },
+  {
+    "revision": "148406030c0320adaa3f",
+    "url": "/CT-2025-UI/static/js/main.14840603.chunk.js"
+  },
+  {
+    "revision": "19db717f2686ce361ba4",
+    "url": "/CT-2025-UI/static/js/runtime~main.19db717f.js"
+  },
+  {
+    "revision": "8ee6429e7fa952ffaaa6",
+    "url": "/CT-2025-UI/static/css/2.55b8303f.chunk.css"
+  },
+  {
+    "revision": "8ee6429e7fa952ffaaa6",
+    "url": "/CT-2025-UI/static/js/2.8ee6429e.chunk.js"
+  },
+  {
+    "revision": "0cfe130a2153dc22f41958d30276e21f",
+    "url": "/CT-2025-UI/index.html"
+  }
+];
